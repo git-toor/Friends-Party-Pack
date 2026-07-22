c953659c534e7f648f1143b6a2ee373175f2efca
Pranav Toor <pranavtoor@hotmail.com>
2026-07-22 23:41:15 +0200

    Remove card translucency when not player's turn — cards always full opacity

--- client/src/games/exploding-kittens/ExplodingKittensGame.tsx
+++ client/src/games/exploding-kittens/ExplodingKittensGame.tsx
@@ -396,7 +396,7 @@ export default function ExplodingKittensGame({
           cards={gs.myHand}
           selectedCardIds={selectedCardIds}
           onSelectCard={handleSelectCard}
-          disabled={!isMyTurn || turnPhase !== 'playing'}
+          disabled={false}
         />
       </div>
 
